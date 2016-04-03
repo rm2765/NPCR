bdccf7ca9b74538a1e2c093d1dea08bcc1d54891
rm2765 <rm2765@nyu.edu>
2016-04-03 16:39:38 -0400

    NPCR Final Assignment

---
+++ js/script.js
@@ -0,0 +1,12 @@
+cartodb.createVis(
+	'map',
+	'https://rm2765.cartodb.com/api/v2/viz/e2d3b802-f854-11e5-80df-0e31c9be1b51/viz.json'
+	)
+
+ // cartodb.createLayer(map, 'https://rm2765.cartodb.com/api/v2/viz/4c06f3d6-ecaa-11e5-9b95-0ef24382571b/viz.json', { https: true })
+ //    .addTo(map)
+ //    .on('done', function(layer) {
+
+ //    }).on('error', function() {
+ //      //log the error
+ //    });
